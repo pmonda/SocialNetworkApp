db3e354c23b5755d10fa997e9973960f025d9484
pmonda <praneymonda@gmail.com>
2025-01-28 16:04:28 -0500

    SNA-3 Update auth.py

--- auth.py
+++ auth.py
@@ -1,12 +1,16 @@
 #authentication module for social media app
 
-
+accounts = {} # key: username, value: hashed password
 
 def signup():
-  pass
+  input("What is your username")
+  input("Create your password")
 
-def login():
-  pass
+def login(username ,pwd):
+  authenticate_user(pwd)
   
-def authenticate_user():
-  pass
+def authenticate_user(credential):
+  if credential == accounts[username]:
+      print("successful login")
+      return
+  print("unsuccessful login attempt, try again.")
